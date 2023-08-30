3b5bc1042bb562e47be7dc5cfcc7472f654f21ab
gabriel <ga.dearaujo4@gmail.com>
2023-08-30 01:54:34 -0300

    Adicionado Classe sobre Nullable

---
+++ src/main/kotlin/fundamentos/nullsafety.kt
@@ -0,0 +1,17 @@
+package fundamentos
+fun main() {
+
+    var lista : List<Int?> = listOf(1, 2, null, 4)
+    var listaNullable : List<Int>? = null
+
+//    var nome : String? = null
+//
+//    var tamanho : Int = nome?.length ?: 0 // Elvis operator --> Caso for nulo, ele substitui o valor do 0 [Referente ao ?? {valor} em Flutter]
+//
+//    println(tamanho)
+
+//    var nome : String? = null
+//
+//    val toShort : Short = nome!!.length.toShort() // !! Dá a certeza de que o valor nunca será nulo
+
+}
