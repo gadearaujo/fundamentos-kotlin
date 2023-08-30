dcede886670b4aebdb5a9474d5e4966d32dab31d
gabriel <ga.dearaujo4@gmail.com>
2023-08-29 22:37:23 -0300

    Adicionado Classe sobre variaveis

---
+++ src/main/kotlin/fundamentos/variaveis.kt
@@ -0,0 +1,11 @@
+package fundamentos
+fun main() {
+    var nome = "Gabriel" // valor mutável
+    val nomeVal = "Gabriel" // valor imutável
+}
+class variaveis {
+   lateinit var teste: String
+   fun iniciaVariaveis() {
+       teste = "Teste"
+   }
+}
